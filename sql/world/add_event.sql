7c6d24202ee5a6484fdd8924a3b814b495109a2f
jackhein <moustiquejunior@gmail.com>
2023-03-20 00:03:08 +0100

    Correct insert of TIMESTAMP data

--- sql/world/add_event.sql
+++ sql/world/add_event.sql
@@ -1,5 +1,5 @@
 INSERT INTO game_event (
 `eventEntry`, `start_time`,        `end_time`,          `occurence`, `length`, `holiday`, `holidayStage`, `description`,   `world_event`, `announce`
 ) VALUES (
-46,           2023-03-04 00:00:01, 2030-12-31 12:00:00, 10080,       3060,     0,         0,              "Weekend Bonus", 0,             0
+46,           "2023-03-04 00:00:01", "2030-12-31 12:00:00", 10080,       3060,     0,         0,              "Weekend Bonus", 0,             0
 );
